ec0e44cbd598ad307f2c73304f35dc33c65bd0fe
Henrik H. <henrik.hinterland@gmail.com>
2025-04-15 02:06:11 +0200

    added circle and triangle

--- assets.js
+++ assets.js
@@ -3,21 +3,35 @@ function drawTest(){
     ctx.fillRect(50, 50, 100, 100);
 }
 
-function drawCross(){
-    const centerX = canvas.width / 2;
-    const centerY = canvas.height / 2;
-    const size = 50;
-
+function drawPlayGround(){
     ctx.strokeStyle = 'black';
-    ctx.lineWidth = 5;
-
+    ctx.lineWidth = 3;
+    var f = 100;
+    //triangle
     ctx.beginPath();
-    ctx.moveTo(centerX, centerY - size);
-    ctx.lineTo(centerX, centerY + size);
+    ctx.moveTo(1*f,0*f);
+    ctx.lineTo(2*f,1*f);
+    ctx.lineTo(0*f,1*f);
+    ctx.lineTo(1*f,0*f);
     ctx.stroke();
-
+}
+function drawTriangle(size,x,y,color){
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 3;
+    var f = size;
     ctx.beginPath();
-    ctx.moveTo(centerX - size, centerY);
-    ctx.lineTo(centerX + size, centerY);
+    ctx.moveTo(1*f+x,0*f+y);
+    ctx.lineTo(2*f+x,1*f+y);
+    ctx.lineTo(0*f+x,1*f+y);
+    ctx.lineTo(1*f+x,0*f+y);
+    ctx.stroke();
+}
+
+function drawCircle(size, x, y, color) {
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 3;
+    var radius = size;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
     ctx.stroke();
 }
